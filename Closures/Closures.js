03e2bd731569d9613de368a60585b8b5672c998f
FAKIRI ISMAIL <59510748+Fakiri-ismail@users.noreply.github.com>
2023-02-21 10:07:40 +0100

    Closures in JavaScript

---
+++ Closures/Closures.js
@@ -0,0 +1,37 @@
+"use strict";
+
+document.getElementById("code").innerHTML = "Learning JavaScript - Closures";
+
+let counter = function(){
+    let count = 0;
+    let increment = function(){
+        return count += 1;
+    }
+    return increment
+}
+
+let firstCounter = counter()
+
+for(let i = 0; i < 10; i++){
+    console.log(firstCounter())     // 1 2 ... 10
+}
+
+let secondCounter = counter()
+
+console.log(secondCounter())        // 1
+console.log(firstCounter())         // 11
+
+
+window.onload = function(){
+    let count = 0
+    let incButton = document.getElementById("increment")
+    incButton.onclick = function(){
+        count += 1
+        document.getElementById("value").innerHTML = count
+    }
+    let decButton = document.getElementById("dicrement")
+    decButton.onclick = function(){
+        count -= 1
+        document.getElementById("value").innerHTML = count
+    }
+}
