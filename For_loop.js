681e63432ac39d983c0d2c4da69df712ce558f83
FAKIRI ISMAIL <59510748+Fakiri-ismail@users.noreply.github.com>
2023-02-23 12:07:49 +0100

    Delete For_loop.js

--- For_loop.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let list = ["h", 2, "jjk", true, NaN, null, undefined]
-for(let i=0; i< list.length; i++){
-    console.log(list[i])
-}
-
-// other way
-for (let l of list) console.log(l)
